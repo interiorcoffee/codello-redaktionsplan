74de293cfc618dcd297adcd20a8037d7b3f3c565
interiorcoffee <hello@interior-coffee.com>
2026-09-22 13:59:10 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Hier die Daten aus Supabase eintragen (Project Settings > API bzw. API Keys)
+window.PLAN_CONFIG = {
+  client: "CODELLO",
+  url: "https://hvrilkteccxydauhhagn.supabase.co",
+  key: "sb_publishable_-_nk1BKxmdZOp6nCt6FxFw_a98bmuek" 
+};
